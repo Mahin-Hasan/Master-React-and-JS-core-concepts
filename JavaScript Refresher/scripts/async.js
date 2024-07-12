70787f33897e04feb12b40a07cca075db4d5c2ea
Mahin Hasan <mahinmtrs@gmail.com>
2024-07-12 08:16:01 +0600

    explored asynchronus js and created 25 react project starter vite template

---
+++ JavaScript Refresher/scripts/async.js
@@ -0,0 +1,12 @@
+async function getData() {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await response.json();
+    console.log(data);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+getData()
