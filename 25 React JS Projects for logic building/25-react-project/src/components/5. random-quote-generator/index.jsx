9b659b6b11556710173f6acdfdb5a1eafaaffc36
Mahin Hasan <mahinmtrs@gmail.com>
2024-07-13 10:05:36 +0600

    created a random quote generator using api

---
+++ 25 React JS Projects for logic building/25-react-project/src/components/5. random-quote-generator/index.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import './quote.css'
+
+const RandomQuoteGenerator = () => {
+    const [loading, setLoading] = useState(false);
+    const [quote, setQuote] = useState(null)
+
+    function getRandomIndex() {
+        return Math.floor(Math.random() * 16);
+    }
+
+
+    async function fetchQuote() {
+
+        try {
+            setLoading(true)
+            const apiResponse = await fetch('https://type.fit/api/quotes', {
+                method: 'GET'
+            });
+            const result = await apiResponse.json();
+            // console.log(result[getRandomIndex()]?.text);
+            // const output = result[getRandomIndex()]?.text;
+            const output = result[getRandomIndex()];
+            console.log(output);
+
+            if (output) {
+                setLoading(false)
+                setQuote(output)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchQuote();
+    }, [])
+    function handleRefresh() {
+        fetchQuote();
+    }
+
+    return (
+        <div className='random-quote-generator'>
+            <h2>Random Quote Generator</h2>
+            <div className="quote-wrapper">
+                {loading ? <h3>Loading Quote...</h3> : quote && <div><p>Author: {quote?.author}</p>
+                   <p> Quote: {quote?.text}</p>
+                </div>
+                }
+                <button onClick={handleRefresh}>Refresh</button>
+            </div>
+        </div>
+    );
+};
+
+export default RandomQuoteGenerator;
